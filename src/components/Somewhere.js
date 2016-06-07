32361e3ba3c6433f48f722a47b9e44a892e451b0
Didier Franc <mail@didierfranc.com>
2016-06-07 23:04:06 +0200

    Create Somewhere.js

---
+++ src/components/Somewhere.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Somewhere = () => (
+  <div>
+    <p>Somewhere !</p>
+    <Link to="/">Go back to root</Link>
+  </div>
+)
+
+export default Somewhere
