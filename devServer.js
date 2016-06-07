55119426628ac8e51fc2e0c513355778b2509f34
Didier Franc <mail@didierfranc.com>
2016-06-07 23:01:25 +0200

    Update devServer.js

--- devServer.js
+++ devServer.js
@@ -6,6 +6,6 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(3000, function (err, result) {
   if (err) console.log(err)
 })
