0c9df9b0225a734f2b5a19191461d17fce11bcae
Didier Franc <mail@didierfranc.com>
2016-06-07 23:05:42 +0200

    Update store.js

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const defaultState = {
-  loaded: false
+  data: {}
 }
 
 export const store = createStore(rootReducer, defaultState,
