0d84821e41528d76532125c909ba31dfa60db946
Didier Franc <mail@didierfranc.com>
2016-06-07 23:02:03 +0200

    Update actionCreators.js

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,15 +1,11 @@
 const get = u => fetch(u).then(r => r.json())
 
-const afterLoad = (data) => {
-  return {
-    type: 'LOAD_DATA',
-    data
-  }
-}
+const afterLoad = (data) => ({
+  type: 'LOAD_DATA',
+  data
+})
 
-export const load = (url) => {
-  return async(dispatch) => {
-    let data = await get(url)
-    dispatch(afterLoad(data))
-  }
+export const load = (url) => async(dispatch) => {
+  let data = await get(url)
+  dispatch(afterLoad(data))
 }
