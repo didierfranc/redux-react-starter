073baddb095d0e2a2f517a17e26e55a5f4969723
Didier Franc <mail@didierfranc.com>
2016-06-07 23:05:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import { history, store } from './store'
 import { Provider } from 'react-redux'
 
 import App from './components/App'
+import Somewhere from './components/Somewhere'
 
 const Index = () => (
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App} />
+      <Route path="somewhere" component={Somewhere} />
     </Router>
   </Provider>
 )
