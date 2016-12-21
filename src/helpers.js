c7ca92bb5ecdc3a9c49a76aedcd2e8ca7db8558d
igor13127 <igor.eschalier@gmail.com>
2016-12-21 15:06:27 +0100

    Add generic post helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,19 @@
 export const storeToken = token => sessionStorage.setItem('token', token)
 export const getToken = () => sessionStorage.getItem('token')
 export const clearToken = () => sessionStorage.removeItem('token')
+
+export const post = async ({ url, body, dispatch, success, failure }) => {
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const data = await res.json()
+    dispatch({ type: success, data })
+  } catch (e) {
+    dispatch({ type: failure })
+  }
+}
