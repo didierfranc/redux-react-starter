46b98b19d5783d86d583947295d24997708d9d85
Didier Franc <didierfranc@me.com>
2017-01-09 18:38:19 +0100

    Use new redux-devtools-extension API

--- src/store.js
+++ src/store.js
@@ -3,10 +3,11 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import { loginWithToken } from './actions'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export const store = createStore(rootReducer, undefined,
-  compose(
+  composeEnhancers(
     applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
   ),
 )
 
