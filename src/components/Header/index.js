aa0621dcbfcb776f338a97dc8bfb80f7e61c103a
chonthicha <punthira.dits@gmail.com>
2017-10-15 16:15:23 +0700

    AddHeaderNavBar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-import { Title } from '../Styled'
+import { Title, Head } from '../Styled'
 import Github from './Github'
 
 const Header = () => (
   <div>
+    <Head />
     <Title>redux-react-starter</Title>
     <Github />
   </div>
